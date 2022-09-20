c297930453e97175ae8ea3ffd470a519e516d23a
origami <mcxyml123@163.com>
2022-09-20 11:41:48 +0800

    build: 引入vxe-table

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import ElementPlus from "element-plus";
 import 'element-plus/dist/index.css'
 import * as Icons from "@element-plus/icons-vue";
 import '@/permission'
+import VXETable from "vxe-table";
+import 'vxe-table/lib/style.min.css'
 
 // tailwindcss引入
 import './index.css'
@@ -17,6 +19,8 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(ElementPlus)
+// 全局引入,生产环境按需引入
+app.use(VXETable)
 
 for (const [key, component] of Object.entries(Icons)) {
     app.component(key, component)
